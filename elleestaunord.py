31ae6a765adcfd6911261bf9c7e2b99f44abfe4b
Eleonore9 <eleonore.mayola@ymail.com>
2013-04-03 13:38:39 +0200

    projects added, modif to aboutme

--- elleestaunord.py
+++ elleestaunord.py
@@ -18,6 +18,10 @@ def index():
 def aboutme():
 	return render_template("aboutme.html")
 
+@app.route("/projects")
+def projects():
+	return render_template("projects.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)
 
