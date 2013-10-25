90f2c62edd4dc7e56bf826c0431b5947bdcd5e59
Eleonore9 <eleonore.mayola@ymail.com>
2013-10-25 17:02:40 +0200

    added a rotated banner but it's not responsive

---
+++ static/js/ele.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.hire_banner').click(function(){
+		alert(message);
+	});
+});
+
+var message = "Hi! I have skills in Python, Ruby, JS, jQuery, HTML & CSS and I'm looking for new professional challenges!"
