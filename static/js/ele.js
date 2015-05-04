8ad8f732567df2c8429841a2ef69e7691eec02c0
Eleonore9 <eleonore.mayola@ymail.com>
2015-05-04 02:18:28 +0100

    update bootstrap, added leaflet map, style bugs to fix

--- static/js/ele.js
+++ static/js/ele.js
@@ -1,7 +1,13 @@
-$(document).ready(function(){
-	$('.hire_banner').click(function(){
-		alert(message);
-	});
-});
+var map;
 
-var message = "Hi! I have skills in Python, Ruby, JS, HTML/CSS. I build websites/apps and am interested in Data Science!"
+function initmap() {
+
+  map = new L.Map('map');
+
+  var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+  }).addTo(map);
+  map.setView([51.5020, -0.1239], 11);
+}
+
+
