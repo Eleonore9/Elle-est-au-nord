eca7accc83db8019486806bff713b20c48622972
Eleonore9 <eleonore.mayola@ymail.com>
2015-05-06 23:53:50 +0100

    Functional version without the map

--- static/js/ele.js
+++ static/js/ele.js
@@ -11,3 +11,23 @@ function initmap() {
 }
 
 
+$( document ).ready(function() {
+
+  var path = window.location.pathname;
+
+  if(path=="/"){
+    $('.nav li.portfolio').removeClass('active');
+    $('.nav li.aboutme').removeClass('active')
+    $('.nav li.home').addClass('active');
+  } else if(path=="/projects"){
+    $('.nav li.home').removeClass('active');
+    $('.nav li.aboutme').removeClass('active')
+    $('.nav li.portfolio').addClass('active');
+  } else if(path=="/aboutme"){
+    $('.nav li.portfolio').removeClass('active');
+    $('.nav li.home').removeClass('active')
+    $('.nav li.aboutme').addClass('active');
+  }
+
+
+});
