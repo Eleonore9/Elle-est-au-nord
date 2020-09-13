7b0d37b8158f5fe089664e6c86712bf3b105e8df
Éléonore Mayola <eleonore.mayola@gmail.com>
2020-09-13 20:53:59 +0200

    Minor style change on Ele mag button hover

--- elleestaunord/site.py
+++ elleestaunord/site.py
@@ -29,5 +29,5 @@ def projects():
 	return render_template("projects.html")
 
 if __name__ == "__main__":
-	#port = int(os.environ.get("PORT", 5000))
+	# port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
