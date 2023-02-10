93045da8189126f71493e0869f952813f3ab6a97
Éléonore Mayola <eleonore.mayola@gmail.com>
2023-02-10 18:52:57 +0100

    Run static 'freeze' app

--- elleestaunord/site.py
+++ elleestaunord/site.py
@@ -4,25 +4,22 @@ from flask import Flask, render_template
 
 
 port = int(os.environ.get('PORT', 33507))
-#heroku config:add PORT=33507
 
 app = Flask(__name__)
 app.config.update(
-    DEBUG = True,
+    DEBUG = False,
 )
 
-# Simple site: just 3 routes for the Hone page and 2 pages
+# Simple site: just 3 routes for the Home page and 2 pages
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-# @app.route("/hireme", methods=["GET"])
-# def hireme():
-#         return render_template("hireme.html")
 
 @app.route("/aboutme/", methods=["GET"])
 def aboutme():
     return render_template("aboutme.html")
+
 
 @app.route("/projects/", methods=["GET"])
 def projects():
--- freeze.py
+++ freeze.py
@@ -1,10 +1,13 @@
 #!/env/bin/python3.8
+import os
 from flask_frozen import Freezer
 from elleestaunord.site import app
 
 
 freezer = Freezer(app)
+port = int(os.environ.get('PORT', 33507))
 
 
 if __name__ == '__main__':
     freezer.freeze()
+    freezer.run(port=port, debug=False)
