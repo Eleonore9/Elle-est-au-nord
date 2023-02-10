ddf11bc78d10de2fc5f82ee40d264ac7b009d335
Éléonore Mayola <eleonore.mayola@gmail.com>
2023-02-10 19:11:27 +0100

    Fix Netlify build error

--- freeze.py
+++ freeze.py
@@ -5,9 +5,9 @@ from elleestaunord.site import app
 
 
 freezer = Freezer(app)
-port = int(os.environ.get('PORT', 33507))
+# port = int(os.environ.get('PORT', 33507))
 
 
 if __name__ == '__main__':
     freezer.freeze()
-    freezer.run(port=port, debug=False)
+    # freezer.run(port=port, debug=False)
