c91a29f5d527256caf815738eb4fc2c3d668edc5
Éléonore Mayola <eleonore.mayola@gmail.com>
2023-11-30 16:40:01 +0100

    Fix the map by updatinh leaflet CDN

--- elleestaunord/static/js/map.js
+++ elleestaunord/static/js/map.js
@@ -6,10 +6,7 @@ function initmap() {
   map = new L.Map('map');
 
   var stamenLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>
-&copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a>
-&copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>
-&copy; <a href="https://www.openstreetmap.org/about/" target="_blank">OpenStreetMap contributors</a>'
+  attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/about/" target="_blank">OpenStreetMap contributors</a>'
   }).addTo(map);
-  map.setView([48.8587, 2.3429], 14);
+  map.setView([48.8587, 2.3429], 13);
 }
